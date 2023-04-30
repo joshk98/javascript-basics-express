bbe3f3166c99b9eae3509e3e16e28fdfee2aff71
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-04-30 14:24:52 +0100

    Finished booleans

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const strings = require('./lib/strings');
 const numbers = require('./lib/numbers');
+const booleans = require('./lib/booleans');
 
 const app = express();
 app.use(express.json());
@@ -19,11 +20,11 @@ app.get('/strings/lower/:string', (req, res) => {
 });
 
 app.get('/strings/first-characters/:string', (req, res) => {
-  const str = req.params.string;
+  const { string } = req.params;
   const length = +req.query.length;
   const result = Number.isNaN(length)
-    ? strings.firstCharacters(str, 1)
-    : strings.firstCharacters(str, length);
+    ? strings.firstCharacters(string, 1)
+    : strings.firstCharacters(string, length);
 
   res.json({ result });
 });
@@ -92,6 +93,33 @@ app.post('/numbers/remainder', (req, res) => {
     res.status(400).json({ error: 'Parameters must be valid numbers.' });
   }
   res.status(200).json({ result: numbers.remainder(a, b) });
+});
+
+app.post('/booleans/negate', (req, res) => {
+  const { value } = req.body;
+  res.status(200).json({ result: booleans.negate(value) });
+});
+
+app.post('/booleans/truthiness', (req, res) => {
+  const { value } = req.body;
+  res.status(200).json({ result: booleans.truthiness(value) });
+});
+
+app.get('/booleans/is-odd/:number', (req, res) => {
+  const number = +req.params.number;
+  if (Number.isNaN(number)) {
+    res.status(400).json({ error: 'Parameter must be a number.' });
+  }
+  res.status(200).json({ result: booleans.isOdd(number) });
+});
+
+app.get('/booleans/:string/starts-with/:char', (req, res) => {
+  const { string } = req.params;
+  const { char } = req.params;
+  if (+char.length > 1) {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  }
+  res.status(200).json({ result: booleans.startsWith(char, string) });
 });
 
 module.exports = app;
